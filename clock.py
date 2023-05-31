c336ff66c6aa5b747eeb52916d1176753863244c
yusuferdemnacar <yusuferdemnacar@gmail.com>
2023-05-31 04:07:03 +0300

    Made changes related to time precision for windows

--- clock.py
+++ clock.py
@@ -8,38 +8,37 @@ import random
 
 class Clock(mp.Process):
 
-    def __init__(self, id, clock_period, broadcast_number, alpha, precision, clock_queue, receive_clock_queue, broadcast_clock_queue, diff_queue):
+    def __init__(self, id, clock_period, broadcast_number, alpha, clock_queue, receive_clock_queue, broadcast_clock_queue, diff_queue):
         super(Clock, self).__init__()
         self.id = id
         self.clock_period = clock_period
         self.broadcast_number = broadcast_number
         self.alpha = alpha
-        self.precision = precision
         self.clock_queue = clock_queue
         self.receive_clock_queue = receive_clock_queue
         self.broadcast_clock_queue = broadcast_clock_queue
         self.diff_queue = diff_queue
 
     def run(self):
-        start = time.time()
+        rising_edge = time.time()
         shift = 0
         diff = 0
         i = 0
         while True:
-            rising_edge = time.time()
+            op_start = time.perf_counter()
             self.clock_queue.put(rising_edge)
-            if i % self.broadcast_number == 0:
-                self.broadcast_clock_queue.put(rising_edge)
             if not self.receive_clock_queue.empty():
                 self.receive_clock_queue.put(None)
                 time_stamps = list(iter(self.receive_clock_queue.get, None))
                 diff = sum([(((time_stamp - rising_edge) % self.clock_period) - self.clock_period) if abs(((time_stamp - rising_edge) % self.clock_period) - self.clock_period) < self.clock_period / 2 else ((time_stamp - rising_edge) % self.clock_period) for time_stamp in time_stamps]) / len(time_stamps)
                 self.diff_queue.put(diff / (self.clock_period / 2))
                 shift = self.alpha * diff
-                if abs(diff) < self.precision * self.clock_period:
-                    shift = 0
             i = i + 1
-            time.sleep(((rising_edge - time.time()) % self.clock_period) + shift)
+            time.sleep((op_start - time.perf_counter()) % self.clock_period + shift)
+            rising_edge = rising_edge + time.perf_counter() - op_start
+            if i % self.broadcast_number == 0:
+                self.broadcast_clock_queue.put(rising_edge)
+                i = 0
             
 class Communicator(mp.Process):
 
@@ -53,7 +52,7 @@ class Communicator(mp.Process):
         while True:
             s = self.broadcast_clock_queue.get()
             for receive_clock_queue in self.receive_clock_queues:
-                if receive_clock_queues.index(receive_clock_queue) != self.id:
+                if self.receive_clock_queues.index(receive_clock_queue) != self.id:
                     receive_clock_queue.put(s)
 
 def update_plot(frames, ax1, ax2, clock_queues, diff_queues, start, clock_period):
@@ -79,7 +78,6 @@ def update_plot(frames, ax1, ax2, clock_queues, diff_queues, start, clock_period
             ydata = np.append(ydata, diff * 100)
             line.set_data(xdata, ydata)
             ax2.set_xlim(xdata[-1] - 90 * clock_period, xdata[-1] + 10 * clock_period)
-
 
 def plot(clock_period, clock_queues, diff_queues):
 
@@ -111,7 +109,7 @@ def plot(clock_period, clock_queues, diff_queues):
 
     start = time.time()
 
-    anim = animation.FuncAnimation(fig, update_plot, fargs=(ax1, ax2, clock_queues, diff_queues, start, clock_period), interval=clock_period/4, blit=False, cache_frame_data=False)
+    anim = animation.FuncAnimation(fig, update_plot, fargs=(ax1, ax2, clock_queues, diff_queues, start, clock_period), interval = 100, blit=False, cache_frame_data=False)
 
     plt.show()
     
@@ -123,13 +121,11 @@ if __name__ == '__main__':
     parser.add_argument('--clockperiod', '-clkp', type=float, help='Clock period in seconds', default=1.0)
     parser.add_argument('--broadcastnumber', '-bn', type=float, help='Broadcast period in number of clock cycles', default=1.0)
     parser.add_argument('--alpha', '-a', type=float, help='Clock skew', default=1.0)
-    parser.add_argument('--precision', '-p', type=float, help='Synchonization precision', default=0.01)
     args = parser.parse_args()
 
     clock_period = args.clockperiod
     broadcast_number = args.broadcastnumber
     alpha = args.alpha
-    precision = args.precision
 
     clock_queues = []
     for i in range(n):
@@ -149,7 +145,7 @@ if __name__ == '__main__':
 
     clocks = []
     for i in range(n):
-        clocks.append(Clock(i, clock_period, broadcast_number, alpha, precision, clock_queues[i], receive_clock_queues[i], broadcast_clock_queues[i], diff_queues[i]))
+        clocks.append(Clock(i, clock_period, broadcast_number, alpha, clock_queues[i], receive_clock_queues[i], broadcast_clock_queues[i], diff_queues[i]))
 
     communicators = []
     for i in range(n):
