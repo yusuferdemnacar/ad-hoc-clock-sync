ef6124c0683916a352f031e6a7068e328ff58e81
yusuferdemnacar <yusuferdemnacar@gmail.com>
2023-06-01 01:20:59 +0300

    Plotter is now able to forget clocks

--- plot.py
+++ plot.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 import time
 import socket
 import argparse
+from collections import OrderedDict
 
 class Listener(mp.Process):
 
@@ -32,7 +33,7 @@ class ClockPlotter(mp.Process):
         self.clock_period = clock_period
         self.clock_queue = clock_queue
         self.diff_queue = diff_queue
-        self.known_ips = []
+        self.known_ips = OrderedDict()
 
         self.fig = plt.figure()
         self.ax1 = self.fig.add_subplot(2,1,1)
@@ -48,7 +49,7 @@ class ClockPlotter(mp.Process):
         self.ax2.set_ylim(-110, 110)
         self.ax2.set_xlabel('Time (s)')
         self.ax2.set_ylabel('% of clock period')
-        self.ax2.set_title('Average Difference')
+        self.ax2.set_title('Average Difference to Other Clocks')
         self.ax2.grid(True)
         self.ax2.xaxis.set_tick_params(rotation=45)
 
@@ -56,11 +57,13 @@ class ClockPlotter(mp.Process):
 
     def run(self):
 
-        anim = animation.FuncAnimation(self.fig, self.animate, interval = 100, blit=False, cache_frame_data=False)
+        start = time.time()
+
+        anim = animation.FuncAnimation(self.fig, self.animate, fargs=(start,), interval = 100, blit=False, cache_frame_data=False)
 
         plt.show()
 
-    def animate(self, frames):
+    def animate(self, frames, start):
 
         clock_time_stamps = []
         diff_time_stamps = []
@@ -75,29 +78,58 @@ class ClockPlotter(mp.Process):
 
         for clock_time_stamp in clock_time_stamps:
             if clock_time_stamp[0] not in self.known_ips:
-                self.known_ips.append(clock_time_stamp[0])
+                self.known_ips[clock_time_stamp[0]] = 0
                 self.ax1.step([], [], where='pre')
                 self.ax2.plot([], [])
 
+        for ip in list(self.known_ips.keys()):
+            if ip not in [clock_time_stamp[0] for clock_time_stamp in clock_time_stamps]:
+                self.known_ips[ip] = self.known_ips[ip] + 1
+            if self.known_ips[ip] > 40:
+                line_index = list(self.known_ips.keys()).index(ip)
+                del self.known_ips[ip]
+                self.ax1.get_lines()[line_index].remove()
+                self.ax2.get_lines()[line_index].remove()
+                if len(self.ax2.get_lines()) == 1:
+                    self.ax2.get_lines()[0].set_data([], [])
+
         for clock_time_stamp in clock_time_stamps:
-            line = self.ax1.lines[self.known_ips.index(clock_time_stamp[0])]
+            line = self.ax1.get_lines()[list(self.known_ips.keys()).index(clock_time_stamp[0])]
             xdata, ydata = line.get_data()
-            xdata = np.append(xdata, clock_time_stamp[1])
+            for i in range(len(xdata)):
+                xdata[i] = xdata[i] - self.clock_period
+            xdata = np.append(xdata, clock_time_stamp[1] % self.clock_period + 9 * self.clock_period)
             ydata = np.append(ydata, 0)
-            xdata = np.append(xdata, clock_time_stamp[1] + self.clock_period / 2)
+            xdata = np.append(xdata, clock_time_stamp[1] % self.clock_period + 9 * self.clock_period + self.clock_period / 2)
             ydata = np.append(ydata, 1)
+            if len(xdata) > 20:
+                xdata = xdata[2:]
+                ydata = ydata[2:]
             line.set_data(xdata, ydata)
-            self.ax1.set_xlim(xdata[-1] - 4.5 * self.clock_period, xdata[-1] + 0.5 * self.clock_period)
+            self.ax1.set_xlim(0, self.clock_period * 10)
 
         for diff_time_stamp in diff_time_stamps:
             try:
-                line = self.ax2.lines[self.known_ips.index(diff_time_stamp[0])]
+                line = self.ax2.get_lines()[list(self.known_ips.keys()).index(diff_time_stamp[0])]
                 xdata, ydata = line.get_data()
-                xdata = np.append(xdata, time.time())
+                xdata = np.append(xdata, time.time() - start)
                 ydata = np.append(ydata, diff_time_stamp[1] * 100)
+                if len(xdata) > int(45 / self.clock_period):
+                    xdata = xdata[1:]
+                    ydata = ydata[1:]
                 line.set_data(xdata, ydata)
                 self.ax2.set_xlim(xdata[-1] - 45, xdata[-1] + 5)
-            except:
+                min = 0
+                max = 0
+                for line in self.ax2.get_lines():
+                    _, limit_ydata = line.get_data()
+                    if (len(limit_ydata) > 0) and (np.min(limit_ydata) < min):
+                        min = np.min(limit_ydata)
+                    if (len(limit_ydata) > 0) and (np.max(limit_ydata) > max):
+                        max = np.max(limit_ydata)
+                self.ax2.set_ylim(min - 10, max + 10)
+            except Exception as e:
+                print(e)
                 pass
 
         self.ax1.legend(self.known_ips, loc='upper left')
